f424326a79acc12bc3009cf8f532d58d8b2e8770
Lucas <uzanlah@gmail.com>
2020-11-21 00:35:09 +0100

    squalala

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyABqBbxChawk8qY4rN_RvNixqGQMArcLoQ",
+  authDomain: "tryout-daf57.firebaseapp.com",
+  databaseURL: "https://tryout-daf57.firebaseio.com",
+  projectId: "tryout-daf57",
+  storageBucket: "tryout-daf57.appspot.com",
+  messagingSenderId: "692993983739",
+  appId: "1:692993983739:web:e7e2cc85d36597106a9c5e"
+};
+
+export const environment = {
+  production: false,
+  firebaseConfig: firebaseConfig
+};
